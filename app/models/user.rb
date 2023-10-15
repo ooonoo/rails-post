fed37bf6abf71f789e84994e9ab9e779c5059207
ooonoo <bscnext63@gmail.com>
2023-10-15 23:35:52 +0900

    バリデーションに対するコメントを該当箇所の下部から上部へ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true
   # 空でないことを確認する
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
   # 空でないことを確認する,重複していないことを確認する
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :email, presence: true, uniqueness: true
   # 空でないことを確認する,最低8文字であることを確認する
+  validates :password, presence: true, length: { minimum: 8 }
 end
