61dd3a010dc50efe43b7167b085070506128b47b
ooonoo <bscnext63@gmail.com>
2023-10-16 22:40:11 +0900

    マイグレーションファイルの項目毎にコメントを追加

--- db/migrate/20231011155725_create_users.rb
+++ db/migrate/20231011155725_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
+      t.string :name, comment:"ユーザー名"
+      t.string :email, comment:"メールアドレス"
+      t.string :password, comment:"パスワード"
 
       t.timestamps
     end
