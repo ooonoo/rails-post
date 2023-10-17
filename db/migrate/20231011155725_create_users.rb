4fa1f7ba78f3bd8181619f8cf80e0b883ae307b9
ooonoo <bscnext63@gmail.com>
2023-10-17 23:46:03 +0900

    テーブルにnull: falseを追加し空での保存に制限を追加

--- db/migrate/20231011155725_create_users.rb
+++ db/migrate/20231011155725_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name, comment:"ユーザー名"
-      t.string :email, comment:"メールアドレス"
-      t.string :password, comment:"パスワード"
+      t.string :name, null: false, comment:"ユーザー名"
+      t.string :email, null: false, comment:"メールアドレス"
+      t.string :password, null: false, comment:"パスワード"
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,9 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_10_11_155725) do
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
