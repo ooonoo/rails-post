3c333bd17c8965dfc48c053d8b0850cc183dbda7
ooonoo <bscnext63@gmail.com>
2023-10-20 00:07:42 +0900

    seed.rbにテストデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# 一括で10個のユーザーデータを作成
+# 10回繰り返し処理を行う
+10.times do |n|
+    User.create!(
+      # n + 1で数字が重複しないようにする
+      email: "test#{n + 1}@test.com",
+      name: "テストユーザー#{n + 1}",
+      password: "testhoge"
+    )
+  end
+  
