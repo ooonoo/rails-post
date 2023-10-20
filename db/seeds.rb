62d19582dde8dbb1b2055108904f8eec5d35c262
ooonoo <bscnext63@gmail.com>
2023-10-21 00:47:15 +0900

    rubocop -A db/seeds.rbを実行

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
 # 一括で10個のユーザーデータを作成
 # 10回繰り返し処理を行う
 10.times do |n|
-    User.create!(
-      # n + 1で数字が重複しないようにする
-      email: "test#{n + 1}@test.com",
-      name: "テストユーザー#{n + 1}",
-      password: "testhoge"
-    )
-  end
-  
+  User.create!(
+    # n + 1で数字が重複しないようにする
+    email: "test#{n + 1}@test.com",
+    name: "テストユーザー#{n + 1}",
+    password: 'testhoge'
+  )
+end
