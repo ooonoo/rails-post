87b5fa31e8df37411aa7b61b96949e74a1337d86
ooonoo <bscnext63@gmail.com>
2023-10-23 22:29:24 +0900

    post.rbにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  # 空でないことを確認する
+  validates :contents, presence: true
+  # 空でないことを確認する,最大200文字であることを確認する
+  validates :user_id, presence: true, length: { maximum: 200 }
 end
