d2a6c19cd0a7c0639567319d2599ed184b51803d
ooonoo <bscnext63@gmail.com>
2023-10-23 22:17:32 +0900

    rails db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_155725) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_21_061351) do
+  create_table "posts", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
