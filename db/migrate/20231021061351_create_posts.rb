d52451dbee3a710e6b719f1a85cd0ca190f54772
ooonoo <bscnext63@gmail.com>
2023-10-23 22:10:23 +0900

    マイグレーションファイルにnullfalseとコメントを追加

--- db/migrate/20231021061351_create_posts.rb
+++ db/migrate/20231021061351_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.text :content
-      t.integer :user_id
+      t.text :content, null: false, comment:"投稿内容"
+      t.integer :user_id, null: false, comment:"ユーザーID"
 
       t.timestamps
     end
