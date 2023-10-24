7121b9ad460b4727ef9778242c2320fae63b1b88
ooonoo <bscnext63@gmail.com>
2023-10-25 01:17:57 +0900

    seeds.rbのデータをユーザー毎に10投稿となるように変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
 # 一括で10個のユーザーデータを作成
 # 10回繰り返し処理を行う
 10.times do |n|
-  User.create!(
+  user = User.create!(
     # n + 1で数字が重複しないようにする
     email: "test#{n + 1}@test.com",
     name: "テストユーザー#{n + 1}",
     password: 'testhoge'
   )
+
+  10.times do |m|
+    Post.create!(
+      content: "contenthoge#{n * 10 + m + 1}",
+      user_id: user.id
+    )
+  end
 end
