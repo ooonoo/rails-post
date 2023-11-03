c8d09f58bceeeb12ec3c7ff6f146c3a4ac0af077
ooonoo <bscnext63@gmail.com>
2023-11-04 00:59:53 +0900

    model記述の順序を変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
-  # 空でないことを確認する
-  validates :content, presence: true, length: { maximum: 200 }
-  # 空でないことを確認する,最大200文字であることを確認する
-  validates :user_id, presence: true
-
   belongs_to :user
+
+  # 空でないことを確認する,最大200文字であることを確認する
+  validates :content, presence: true, length: { maximum: 200 }
+  # 空でないことを確認する
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
+  has_many :posts
+
   # 空でないことを確認する
   validates :name, presence: true
   # 空でないことを確認する,重複していないことを確認する
   validates :email, presence: true, uniqueness: true
   # 空でないことを確認する,最低8文字であることを確認する
   validates :password, presence: true, length: { minimum: 8 }
-
-  has_many :posts
 end
