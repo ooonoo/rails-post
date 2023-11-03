23d6a7e9bc2e7ef8e2abb50a71a9b3e40529153a
ooonoo <bscnext63@gmail.com>
2023-11-04 01:31:08 +0900

    ルーティングの記述順序を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   get '/' => 'users#login_form'
+  get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
