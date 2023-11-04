9ac1a5132ae24f81a2d260ffc43da6eb90cac331
ooonoo <bscnext63@gmail.com>
2023-11-04 15:46:04 +0900

    belongs_toの関連付けモデルのバリデーション記述がなくてもバリデーション作用があることを確認

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,4 @@ class Post < ApplicationRecord
 
   # 空でないことを確認する,最大200文字であることを確認する
   validates :content, presence: true, length: { maximum: 200 }
-  # 空でないことを確認する
-  validates :user_id, presence: true
 end
