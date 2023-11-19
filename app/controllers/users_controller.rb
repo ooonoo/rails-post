36b3168eb0cc2ab1246de91d53a14146151cc047
ooonoo <bscnext63@gmail.com>
2023-11-20 08:33:38 +0900

    マイページにメールアドレス、登録名称、個人投稿を表示される内容を作成、おおまかなビューを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   end
 
   def show
+     # 特定のユーザーのIDを指定
+     user_id = 1
+
+    # 特定のユーザーの投稿のみを取得
+     @posts = Post.where(user_id: user_id).order(created_at: :desc)
   end
 end
