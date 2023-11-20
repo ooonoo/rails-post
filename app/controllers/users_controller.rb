3489eec21a3beaf60d9cb4b0c852d2ef12417f08
ooonoo <bscnext63@gmail.com>
2023-11-21 01:05:19 +0900

    マイページのメールアドレスと名前にユーザー情報を取得して表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def show
      # TODO ログイン機能実装後にcurrent_user.idに変更
      user_id = 1
-
+     @user = User.find(user_id)
      @posts = Post.where(user_id: user_id).order(created_at: :desc)
   end
 end
