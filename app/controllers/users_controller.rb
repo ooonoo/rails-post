092e9a2ba22b63e815265a8b2a883d3608e2cb62
ooonoo <bscnext63@gmail.com>
2023-11-30 21:57:56 +0900

    rubocop -a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
   end
 
   def show
-     # TODO ログイン機能実装後にcurrent_user.idに変更
-     user_id = 1
+    # TODO: ログイン機能実装後にcurrent_user.idに変更
+    user_id = 1
      @user = User.find(user_id)
-     @posts = Post.where(user_id: user_id).order(created_at: :desc)
+     @posts = Post.where(user_id:).order(created_at: :desc)
   end
 end
